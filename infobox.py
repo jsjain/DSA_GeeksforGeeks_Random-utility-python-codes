da8ef79b45a711c1e50bcc5cd8c1e6262a3019ba
Shubham Jain <jsjain007@gmail.com>
2017-01-06 12:11:40 +0530

    updated
    works with movies and biography

--- infobox.py
+++ infobox.py
@@ -1,5 +1,6 @@
 from requests import get
 from bs4 import BeautifulSoup
+import re
 
 query = raw_input("enter search string: \n")
 url = "http://en.wikipedia.org/w/api.php?action=opensearch&search=" + query + "&limit=10&namespace=0&format=json"
@@ -19,22 +20,37 @@ req = get(url_to_parse)
 
 # getting the infobox data
 soup = BeautifulSoup(req.content, "html.parser")
-gdata = soup.find_all("table", {"class": "infobox vevent"})
+# gdata = soup.find_all("table", {"class": "infobox vevent"})
+gdata = soup.find_all("table", {"class": "infobox"})
 
 # data = gdata[0].get_text().encode('ascii', 'ignore').replace('\n', ' ').split("  ")
 data = gdata[0].find_all("th", {"scope": "row"})
-value = gdata[0].find_all("td", {"style": "line-height:1.3em;"})
+# value = gdata[0].find_all("td", {"style": "line-height:1.3em;"})
+value = gdata[0].find_all("td")
 newdata = []
 newvalue = []
 
 if data[0].text == "Hindi":
     data.pop(0)
+for v in value:
+    if re.findall("Theatrical release poster", v.text):
+        value.remove(v)
+    elif v.text.encode("ascii", "ignore") == ("" or " "):
+        value.remove(v)
+    else:
+        pass
 for d in data:
-    newdata.append(str(d.text.replace("\n", "")))
+    newdata.append(str(d.text.encode('ascii', 'ignore').replace("\n", "").strip()))
 
 for v in value:
-    newvalue.append(str(v.text.encode('ascii', 'ignore').replace("\n\n", " ")))
-
-
-for d, v in zip(newdata, newvalue):
-    print "\n{0}: {1}".format(d, v)
+    if (v.text != ""):
+        newvalue.append(str(v.text.encode('ascii', 'ignore').replace("\n", "")))
+if len(newdata) == len(newvalue):
+    for d, v in zip(newdata, newvalue):
+        print "\n{0}: {1}".format(d, v)
+else:
+    for d, v in zip(newdata, newvalue[1:]):
+        print "\n{0}: {1}".format(d, v)
+# print newvalue
+# print newdata
+# print len(newdata) == len(newvalue)
