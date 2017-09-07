e497bed773397edf0b760926b2b99802ff16a942
Shubham Jain <jsjain007@gmail.com>
2017-09-07 23:23:42 +0530

    updated

--- Utilities Script/watchChanges.py
+++ Utilities Script/watchChanges.py
@@ -1,26 +1,22 @@
 import sys
 import time
-import logging
+import distutils
 from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
+from watchdog.events import PatternMatchingEventHandler
 
 
-class MyEventHandler(FileSystemEventHandler):
-    def __init__(self, observer):
-        self.observer = observer
-
+class MyEventHandler(PatternMatchingEventHandler):
     def on_any_event(self, event):
         print(event)
-        print(type(event))
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S')
-    path = sys.argv[1] if len(sys.argv) > 1 else '.'
+    path = sys.argv[1] if len(sys.argv) > 1 else '.\\src\\'
+    event_handler = MyEventHandler(regexes=['*.js', '*.less'], 
+                                    ignore_regexes=['*.git'], 
+                                    ignore_directories=False, 
+                                    case_sensitive=False)
     observer = Observer()
-    event_handler = MyEventHandler(observer)
     observer.schedule(event_handler, path, recursive=True)
     observer.start()
     try:
