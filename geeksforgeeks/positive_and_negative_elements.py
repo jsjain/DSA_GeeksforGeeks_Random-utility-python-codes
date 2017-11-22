91cf2bec1a4ce0dac705fa780274ebad00b33e91
Shubham Jain <jsjain007@gmail.com>
2017-11-23 00:33:04 +0530

    kadanes algorithm

--- geeksforgeeks/positive_and_negative_elements.py
+++ geeksforgeeks/positive_and_negative_elements.py
@@ -1,11 +1,11 @@
 t = int(input())
 while t > 0:
-    n= int(input())
-    arr = list(map(int, raw_input().split()))
+    n = int(input())
+    arr = list(map(int, input().split()))
     pos_arr = []
     neg_arr = []
     for i in range(n):
-        if(arr[i] >= 0):
+        if arr[i] >= 0:
             pos_arr.append(arr[i])
         else:
             neg_arr.append(arr[i])
@@ -13,8 +13,8 @@ while t > 0:
     neg_len = len(neg_arr)
     i = j = 0
     print_string = ''
-    while i < pos_len and j < neg_len :
-        print_string += str(pos_arr[i]) + " " + str(neg_arr[j]) + " " 
+    while i < pos_len and j < neg_len:
+        print_string += str(pos_arr[i]) + " " + str(neg_arr[j]) + " "
         i += 1
         j += 1
     while i < pos_len:
@@ -23,5 +23,5 @@ while t > 0:
     while j < pos_len:
         print_string += str(pos_arr[j])
         j += 1
-    print (print_string)
+    print(print_string)
     t -= 1
